5c2acb94ea0893823be4fd76299fc6824078c661
Prince Derrek <derrekkimani@proton.me>
2025-08-19 10:11:57 +0300

    Created new cosnole application

---
+++ Pwm.Cli/VaultModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pwm;
+
+public class VaultItem
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Title { get; set; } = "";
+    public string Url { get; set; } = "";
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Notes { get; set; } = "";
+
+}
+public class VaultData
+{
+    public List<VaultItem> Items { get; set; } = new();
+}
